e250a01bc54c179d1325590c865479acb491ae05
Keifer <keifergilbert@gmail.com>
2022-10-27 16:03:01 -0700

    changed successful blog post message prompt

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -25,7 +25,7 @@ class BlogsController < ApplicationController
 
     respond_to do |format|
       if @blog.save
-        format.html { redirect_to blog_url(@blog), notice: "Oh, you lettin mfers know today huh?" }
+        format.html { redirect_to blog_url(@blog), notice: "Blog successfully posted." }
         format.json { render :show, status: :created, location: @blog }
       else
         format.html { render :new, status: :unprocessable_entity }
